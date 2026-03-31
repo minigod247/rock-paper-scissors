52457171d770e8f99629fab6079d07b7631f0149
minigod247 <minigod247@gmail.com>
2026-03-31 15:25:34 -0700

    Add getHumanChioce & getComputerChoice logic

--- script.js
+++ script.js
@@ -11,6 +11,53 @@ function getComputerChoice() {
     return "scissors";
   }
 }
-
 console.log(getComputerChoice())
 
+
+function getHumanChoice() {
+  let userInput = prompt("What's is your choice?");
+  return userInput;
+}
+console.log(getHumanChoice())
+
+
+function playRound(humanChoice, computerChoice) {
+ humanChoice = humanChoice.toLowerCase();
+
+  if (humanChoice === computerChoice) {
+    return "Draw";
+  } else if (humanChoice === "rock" && computerChoice === "scissors") {
+    humanScore++;
+    return `You win`;
+  } else if (humanChoice === "paper" && computerChoice === "rock") {
+    humanScore++;
+    return `You win`;
+  } else if (humanChoice === "scissors" && computerChoice === "paper") {
+    humanScore++;
+    return `You win`;
+  } else {
+    computerScore++;
+    return `You lose`;
+  }
+}
+ // console.log(playRound())
+ let humanSelection = getHumanChoice();
+ let = computerSelection = getComputerChoice();
+
+ console.log(playRound(humanSelection, computerSelection));
+
+
+function playGame() {
+  for (let i = 0; i < 5; i++) {
+    let humanSelection = getHumanChoice();
+    let computerSelection = getComputerChoice();
+
+    console.log(playRound(humanSelection, computerSelection));
+  }
+
+  console.log("Final Score:");
+  console.log("Human:", humanScore);
+  console.log("Computer:", computerScore);
+}
+
+playGame()
