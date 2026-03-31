cc4a0462fd1230c373049e435a8d636639fead57
minigod247 <minigod247@gmail.com>
2026-03-31 11:34:24 -0700

    Write getHumanChoice logic

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("Hello World!");
+let humanScore = 0;
+let computerScore = 0;
+
+function getComputerChoice() {
+  let number = Math.random();
+  if (number < 0.33) {
+    return "rock";
+  } else if (number < 0.66) {
+    return "paper";
+  } else {
+    return "scissors";
+  }
+}
+
+console.log(getComputerChoice())
+
